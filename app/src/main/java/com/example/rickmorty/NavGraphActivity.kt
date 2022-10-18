ec6366c0df62eb195cf6beaa9fe7eb61e6c79256
Anwar-alsina <Anwarfahad6700@gmail.com>
2022-10-18 12:52:44 +0300

    added a drawerLayout

--- app/src/main/java/com/example/rickmorty/NavGraphActivity.kt
+++ app/src/main/java/com/example/rickmorty/NavGraphActivity.kt
@@ -2,11 +2,14 @@ package com.example.rickmorty
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.navigation.NavigationView
 
 class NavGraphActivity : AppCompatActivity() {
 
@@ -21,10 +24,19 @@ class NavGraphActivity : AppCompatActivity() {
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
 
-        appBarConfiguration = AppBarConfiguration(navController.graph)
+        val drawerLayout = findViewById<DrawerLayout>(R.id.drawer_layout)
+        appBarConfiguration = AppBarConfiguration(
+            topLevelDestinationIds = setOf(R.id.characterListFragment,R.id.episodeFragment),
+            drawerLayout = drawerLayout)
         setupActionBarWithNavController(
             navController = navController,
             configuration = appBarConfiguration
+        )
+
+        findViewById<NavigationView>(R.id.nav_view)
+            .setupWithNavController(navController)
+        findViewById<NavigationView>(
+            navController.graph.startDestinationId
         )
     }
 
